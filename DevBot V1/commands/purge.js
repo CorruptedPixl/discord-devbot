890b36dc8bc08617ee842c785427597d77d6e395
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2019-08-05 20:27:12 +0200

    Handles NaN, Min and Max messages errors

--- DevBot V1/commands/purge.js
+++ DevBot V1/commands/purge.js
@@ -1,17 +1,32 @@
 const Discord = require("discord.js");
+const {msgDeleteDelay} = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
 
     let messageDeleteAmount = parseInt(args[0]);                                            //Sets a variable of amt of msg to delete to use later. Also converts the string from args[0] to a number to be able to add 1 to delete the "!purge" message itself too
     messageDeleteAmount = messageDeleteAmount + 1;
 
-    if (messageDeleteAmount <= 0){
+    if (messageDeleteAmount <= 0){                                                          //Handles an error if the user enters a negative value or 0
         message.channel.send(`You're an absolute dipshit, ${message.member}. You can't purge a negative amount of messages!`)
             .then(msg => {
-                msg.delete(5000);
-            })
+                msg.delete(msgDeleteDelay);
+            });
+    }
+    
+    if (messageDeleteAmount >= 100){                                                        //Handles an error if the user tries to delete more than 100 messages
+        message.channel.bulkDelete(100);
+        message.channel.send(`You can only delete 100 messages at a time ${message.member}.`)
+            .then(msg => {
+                msg.delete(msgDeleteDelay);
+            });
     }
 
+    if (typeof(messageDeleteAmount) != Number){
+        message.channel.send(`How about you try and enter a number this time around? ${message.member}`)
+            .then(msg => {
+                msg.delete(msgDeleteDelay);
+            });
+    }
 
     else {
         message.channel.bulkDelete(messageDeleteAmount);                                        //This actually deletes the messages

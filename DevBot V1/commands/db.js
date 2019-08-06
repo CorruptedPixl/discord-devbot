491676223c7be181e907775e423ba9f27a163204
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2019-08-06 18:22:57 +0200

    Updated db command

--- DevBot V1/commands/db.js
+++ DevBot V1/commands/db.js
@@ -1,16 +1,51 @@
 const Discord = require("discord.js");
-const {msgDeleteDelay} = require('../config.json');
+const {msgDeleteDelay, prefix} = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
+    // let dbEmbed = new Discord.RichEmbed()
+    //     .setDescription("***dbrand***")
+    //     .setColor("#ffbb00")
+    //     .addField("dbrand bot", "***dbrand bot***")
+    //     .addField("Message sent in", message.channel)
+    //     .addField("Time", message.createdAt)
+    //message.channel.send(dbEmbed)
+
     let dbEmbed = new Discord.RichEmbed()
-        .setDescription("***dbrand***")
-        .setColor("#ffbb00")
-        .addField("dbrand bot", "***dbrand bot***")
-        .addField("Message sent in", message.channel)
-        .addField("Time", message.createdAt)
-    message.channel.send(dbEmbed)
+	.setColor('#ffbb00')
+	.setTitle('dbrand.com')
+	.setURL(link)
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Regular field title', 'Some value here')
+	.addBlankField()
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('attachment://../assets/1_50FPS.gif')
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+
+
+
+    let link = "https://dbrand.com/";
+    let arrayLength = args.length;
+
+    console.log(arrayLength);
+
+    if (arrayLength == 0){
+        message.channel.send(dbEmbedLink)
+    }
+
+
+    if (args[0] == "grip"){
+        link = link + "shop/grip/";
+    }
 }
 
+
+
+
+
 module.exports.help = {
-    name: "db"
+    name: "db",
+    description: `Use \`${prefix}db x y\` to post a link in chat to a specific product. For example \`${prefix}db grip op7p\``
 }

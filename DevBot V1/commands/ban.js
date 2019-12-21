2c9803e7b7be40cba89f47c578159a37650ee713
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2019-12-21 18:37:33 +0100

    Added temp code

--- DevBot V1/commands/ban.js
+++ DevBot V1/commands/ban.js
@@ -1,10 +1,17 @@
+/* eslint-disable no-unused-vars */
 const Discord = require("discord.js");
-const { msgDeleteDelay } = require('../config.json');
+const { msgDeleteDelay, prefix } = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
-
+    message.channel.bulkDelete(1);
+    message.channel.send(`This module isn't finished yet. Bug Pixl about it or something. (I head money is a good motivator ;) )`)
+        .then(msg => {
+            msg.delete(msgDeleteDelay);
+        });
 }
 
 module.exports.help = {
-    name: "ban"
+    name: "ban",
+    description: `Usage: ${prefix}ban id reason`,
+    helpimage: "./commands/assets/ban_help.png"
 }

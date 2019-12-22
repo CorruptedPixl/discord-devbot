bbf417ea20e1d7a93eed31433063df3f38430ce3
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2019-12-22 01:35:13 +0100

    Overhauled arg handler, added !db ship (country)

--- DevBot V1/commands/db.js
+++ DevBot V1/commands/db.js
@@ -1,68 +1,128 @@
 const Discord = require("discord.js");
 const { msgDeleteDelay, prefix } = require('../config.json');
+const { shippingArray, howtoArray } = require('../commands/command-config/sitemap-arrays.json');
 
 module.exports.run = async (client, message, args) => {
-    // let dbEmbed = new Discord.RichEmbed()
-    //     .setDescription("***dbrand***")
-    //     .setColor("#ffbb00")
-    //     .addField("dbrand bot", "***dbrand bot***")
-    //     .addField("Message sent in", message.channel)
-    //     .addField("Time", message.createdAt)
-    //message.channel.send(dbEmbed)
+
+    let destination;                                                                                            //Defines global variables
+    let link = "https://dbrand.com/";                                                                           //Sets standard link to add to later
 
 
-    let link = "https://dbrand.com/";
-    let arrayLength = args.length;
-
-
-
-    console.log(arrayLength);
-
-    if (arrayLength == 0) {
+    if (args.length == 0) {                                                                                     //Deletes user message and sends an error if they don't give any args
         message.channel.bulkDelete(1);
-        message.channel.send(`\`Usage: ${prefix}db [arg1] [arg2] [arg3] etc... Min 1 argument expected.\``)
+        message.channel.send(`No argument. Please use !db *argument*`)
     }
 
-    else if (arrayLength == 1) {
 
-        if (args[0] == "grip" || args[0] == "g") {                                                                               //Set link to grip
+    else if (args.length > 0 && args.length < 3) {
+
+        //!Below are the supported functions that handle certain db args
+        const caseGrip = () => {                        //Set link to grip
             link = link + "shop/grip/#grip-devices";
             message.channel.bulkDelete(1);
-            console.log(link);
         }
 
-        if (args[0] == "help" || args[0] == "contact" || args[0] == "support" || args[0] == "h" || args[0] == "c") {             //Set link to support
+        const caseSupport = () => {                     //Set link to support
             link = link + "contact";
             message.channel.bulkDelete(1);
         }
 
-        if (args[0] == "skins" || args[0] == "s") {                                                                              //Set link to skins
+        const caseSkin = () => {                        //Set link to skins
             link = link + "shop/skins";
             message.channel.bulkDelete(1);
         }
 
-        if (args[0] == "prism" || args[0] == "p") {                                                                              //Set link to prism
+        const casePrism = () => {                       //Set link to Prism
             link = link + "shop/prism";
             message.channel.bulkDelete(1);
         }
 
+
+        //!This handles the first argument. Handles it if found, if not, it just stops
+        switch (args[0]) {
+            //Grip
+            case "grip": caseGrip(); break;
+            case "g": caseGrip(); break;
+            //Support
+            case "support": caseSupport(); break;
+            case "help": caseSupport(); break;
+            case "contant": caseSupport(); break;
+            case "h": caseSupport(); break;
+            case "c": caseSupport(); break;
+            //Skins
+            case "skins": caseSkin(); break;
+            case "skin": caseSkin(); break;
+            case "s": caseSkin(); break;
+            //Prism
+            case "prism": casePrism(); break;
+            case "p": casePrism(); break;
+
+            default:
+                break;
+        }
+
+
+        if (args[0] == "shipping" || args[0] == "ship") {                                                       //Checks for "shipping" as first arg, then searches the shipping array for any matches to the second arg and returns the first link found
+
+            let matches;
+            if (args[1] != undefined) {
+                matches = shippingArray.filter(s => s.includes(args[1].toString().toLowerCase()));              //Creates an array with all country's found that match part of the substring (arg1)
+                console.log(args[1].toString());
+
+                message.channel.bulkDelete(1);
+                console.log(matches);                                                                           //Used for debugging
+
+                if (matches.length > 0) {
+                    link = `https://dbrand.com/shipping/${matches[0]}`;
+                    destination = matches[0];
+
+                    let shippingEmbed = new Discord.RichEmbed()
+                        .setTitle(`Shipping to ${destination}`)
+                        .setDescription(`Click the link above to see shipping time to ${destination}. \nYou can also [view all shipping destinations here](https://dbrand.com/shipping)`)
+                        .setURL(`${link}`)
+                        .setColor('#ffbb00')
+                        .setTimestamp()
+                        .attachFiles(['../DevBot V1/commands/assets/db-logo.png'])
+                        .setFooter('dbrand.com', 'attachment://db-logo.png')
+
+                    message.channel.send(shippingEmbed);
+                } else {
+                    link = link + "shipping";
+
+                    let shippingEmbedNotFound = new Discord.RichEmbed()
+                        .setTitle(`Check shipping times here!`)
+                        .setDescription(`Country not found.\nYou can also [view all shipping destinations here](https://dbrand.com/shipping)`)
+                        .setURL(`${link}`)
+                        .setColor('#ffbb00')
+                        .setTimestamp()
+                        .attachFiles(['../DevBot V1/commands/assets/db-logo.png'])
+                        .setFooter('dbrand.com', 'attachment://db-logo.png')
+                    message.channel.send(shippingEmbedNotFound);
+                }
+            }
+        }
+
         let dbEmbedLink = new Discord.RichEmbed()
-            .setColor('#ffbb00')
+
             .setTitle(link)
             .setURL(link)
             .setDescription(link)
-            .setThumbnail('attachment://50FPS.gif')
+            .setThumbnail('attachment://db-logo.png')
             .addField('Regular field title', 'Some value here')
             .addBlankField()
+            .setColor('#ffbb00')
             .addField('Inline field title', 'Some value here', true)
             .addField('Inline field title', 'Some value here', true)
-            .attachFiles(['../DevBot V1/commands/assets/50FPS.gif'])
-            .setImage('attachment://50FPS.gif')
+            .attachFiles(['../DevBot V1/commands/assets/db-logo.png'])
+            .setImage('attachment://db-logo.png')
             .setTimestamp()
-            .setFooter('Some footer text here', 'attachment://./assets/50FPS.gif')
+            .setFooter('dbrand.com', 'attachment://db-logo.png')
 
 
-        message.channel.send(dbEmbedLink);
+        if (args[0] != "shipping" && args[0] != "ship") {
+            message.channel.send(dbEmbedLink);
+        }
+
     }
 
     else {

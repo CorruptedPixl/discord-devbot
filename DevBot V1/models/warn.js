fcfc0e9563a53ab6e92e3f9b7582b21ed755b707
Misly <60405462+Misly16@users.noreply.github.com>
2020-07-08 14:39:40 +0100

    Update warn.js

--- DevBot V1/models/warn.js
+++ DevBot V1/models/warn.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+const warndbSchema = mongoose.Schema({
+
+    _id: mongoose.Schema.Types.ObjectId,
+    userID: String,
+    modID: String, 
+    reason: String
+    
+});
+
+module.exports = mongoose.model("warndb", warndbSchema)
